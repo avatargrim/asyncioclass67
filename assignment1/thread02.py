d56bbd4df3a8339fe76b6788551d34566689152f
punnawat nonthiwatwanit <punnawat1824@gmail.com>
2024-06-19 13:06:50 +0700

    submit assignment

--- assignment1/thread02.py
+++ assignment1/thread02.py
@@ -1,1 +1,20 @@
 # running a function with arguments in another thread
+from time import sleep,ctime
+from threading import Thread
+
+# custom thread class
+class CustomThread(Thread):
+
+    def run(itself):
+        # block for a moment
+        sleep(1)
+        # display a message
+        print(f'{ctime()} This is coming from another thread')
+    
+# create a thread
+thread = CustomThread
+# start the thread
+thread.start()
+# wait for the thread to finish
+print(f'{ctime()} Waiting for thread to finish')
+thread.join()
--- assignment1/thread03.py
+++ assignment1/thread03.py
@@ -1,1 +1,24 @@
 # extending the Thread class
+from time import sleep,ctime
+from threading import Thread
+
+# custom thread class
+class CustomThread(Thread):
+    #overide the fun function
+    def run(self):
+        # block for a moment
+        sleep(1)
+        
+        # display a message
+        print(f'{ctime()} This is coming from another thread')
+    
+# create a thread
+thread = CustomThread()
+
+# run the thread
+thread.start()
+
+# wait for the thread to finish
+print(f'{ctime()} Waiting for thread to finish')
+
+thread.join()
