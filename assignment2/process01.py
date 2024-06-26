076a8796755bc1196c9c3b6ddc2bf4c41a0107dd
punnawat nonthiwatwanit <punnawat1824@gmail.com>
2024-06-26 12:04:08 +0700

    commit assingment 26/6/67

--- assignment2/process01.py
+++ assignment2/process01.py
@@ -1,1 +1,36 @@
 # Multiprocessing 2 kitkens, 2 cooker, 2 dishes
+import multiprocessing
+import os
+from time import sleep, ctime, time
+
+def cooking(index):
+    cooking_time = time()
+    print(f'{ctime()} Kitchen-{index}   : Begin cooking...PID {os.getpid()}')
+    sleep(2)
+    duration = time() - cooking_time
+    print(f'{ctime()} Kitchen-{index}   : Cooking done in {duration:0.2f} seconds!')
+    
+def kitchen(index):
+    cooking(index)
+    
+if __name__=="__main__":
+    # Begin of main thread
+    print(f'{ctime()} Main   : Start cooking...PID {os.getpid()}')
+    start_time = time()  # Corrected line: calling time() to get the start time
+    
+    # Multi kitchens with each chef
+    kitchens = []
+    for index in range(2):
+        p = multiprocessing.Process(target=kitchen, args=(index, ))
+        kitchens.append(p)
+        p.start()
+        
+    for p in kitchens:
+        p.join()
+
+    duration = time() - start_time  # Calculate duration correctly
+    
+    print(f'{ctime()} Main   : Finished Cooking duration in {duration:0.2f} seconds')
+
+    duration = time() - start_time
+    print(f'{ctime()} Main   : Finished Cooking duration in {duration:0.2f} seconds')
--- assignment3/asyncio03.py
+++ assignment3/asyncio03.py
@@ -1,1 +1,5 @@
 # example of creating an event loop
+import asyncio
+
+loop=asyncio.new_event_loop()
+print(loop)
