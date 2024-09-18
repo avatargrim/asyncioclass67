852d9f5c1a2a79f48e74fbc1ed28acb5eeb469ba
punnawat nonthiwatwanit <punnawat1824@gmail.com>
2024-09-18 12:18:42 +0700

    submit bonus

--- bonus/producer.py
+++ bonus/producer.py
@@ -1,1 +1,25 @@
 # https://aiokafka.readthedocs.io/en/stable/
+from aiokafka import AIOKafkaProducer
+import asyncio
+import json
+import random
+
+def serializer(value):
+    return json.dumps(value).encode()
+
+async def send_one():
+    producer = AIOKafkaProducer(
+        bootstrap_servers='localhost:9092')
+    # Get cluster layout and initial topic/partition leadership information
+    await producer.start()
+    while True:
+        try:
+            # Produce message
+            res = await producer.send_and_wait("my_topic", b"Super message")
+            print(res)
+            await asyncio.sleep(1)
+        except:
+            # Wait for all pending messages to be delivered or expire.
+            await producer.stop()
+
+asyncio.run(send_one())
